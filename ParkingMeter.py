2321946441de77f99e22440a2c799002bd0cc59d
bradenzukowski <49949796+bradenzukowski@users.noreply.github.com>
2019-04-24 09:47:04 -0400

    Create ParkingMeter.py

---
+++ ParkingMeter.py
@@ -0,0 +1,7 @@
+class ParkingMeter
+  def __init__(self):
+    self.__minutespurchased = 0
+  def set_minutespurchased(self, value):
+    self.__minutespurchased = value
+  def get_minutespurchased(self):
+    return self.__minutespurchased
