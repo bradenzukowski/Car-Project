3fe89a5165ba0dbf0ec21bbfcbb59ad89d9741a4
bradenzukowski <49949796+bradenzukowski@users.noreply.github.com>
2019-04-24 09:43:35 -0400

    Create Parked Car.py

---
+++ Parked Car.py
@@ -0,0 +1,27 @@
+class ParkedCar():
+ def __init__(self):
+   self.__make=""
+   self.__model=""
+   self.__color = ""
+   self.licenseNumber= ""
+   self.__minutesParked=0
+ def set_minutesParked(self,minutesParked):
+   self.__minutesParked = minutesParked
+ def set_make(self,make):
+   self.__make=make
+ def set_model(self,model):
+   self.__model=model
+ def set_color(self,color):
+   self.__color=color
+ def set_licenseNumber(self,licenseNumber):
+   self.licenseNumber= licenseNumber
+ def get_make(self):
+   return self.__make
+ def get_minutesParked(self):
+   return self.__minutesParked
+ def get_model(self):
+   return self.__model
+ def set_color(self):
+   return self.__color
+ def get_licenseNumber(self):
+   return self.licenseNumber
