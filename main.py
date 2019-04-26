ca6dec91839d46d50cf81976621034cfaeecd600
KadeHilterman <49950149+KadeHilterman@users.noreply.github.com>
2019-04-26 09:14:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from ParkingMeter.py import *
 from ParkingTicket.py import *
 from PoliceOfficer.py import *
 def main():
-  pass
+  car = ParkedCar()
+  meter = ParkingMeter()
+  officer = PoliceOfficer("Jeff", 002)
+  car.set_make("Chevy")
+  car.set_model("1969 Corvette Stingray")
+  car.set_color("Red")
+  car.set_liscenseNumber("YVX-3345")
+  car.set_minutesParked(136)
+  meter.set_minutespurchased(120)
+  ticket = officer.examinecar(car, meter)
+  print(ticket)
 main()
